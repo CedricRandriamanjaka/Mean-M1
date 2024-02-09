a2c1a5ef4b07c97557eaeb2970e08a0281f4b620
CedricRandriamanjaka <randriamanjakacedric@gmail.com>
2024-02-09 15:47:54 +0300

    base de donne

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/services", apiServices)
 app.use("/api/competences", apiCompetences)
 
-var mongoURI = "mongodb+srv://mihobyfahasoavana:crud-express@cluster0.eeg3doq.mongodb.net/mongo-express?retryWrites=true&w=majority";
-// var mongoURI = "mongodb+srv://Cedric:Cedric@meanproject.vuk6uvm.mongodb.net/?retryWrites=true&w=majority";
+// var mongoURI = "mongodb+srv://mihobyfahasoavana:crud-express@cluster0.eeg3doq.mongodb.net/mongo-express?retryWrites=true&w=majority";
+var mongoURI = "mongodb+srv://Cedric:Cedric@meanproject.vuk6uvm.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(mongoURI).then(() => {
     console.log("connection reussit")
