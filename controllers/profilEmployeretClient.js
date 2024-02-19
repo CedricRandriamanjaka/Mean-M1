16328580e46dc066215fdef5eaa94d3c8987dd58
MihobyF <mihobyfahasoavana@gmail.com>
2024-02-19 14:10:47 +0100

    get employees in a competence and in a service

--- controllers/profilEmployeretClient.js
+++ controllers/profilEmployeretClient.js
@@ -1,4 +1,6 @@
 const ProfilEmployerEtClient = require('../models/profilEmployerEtClient');
+const Utilisateur = require('../models/Utilisateur');
+const service = require('../services/service');
 
 async function ajouterProfil(userID, competenceID) {
     try {
@@ -28,8 +30,6 @@ async function ajouterProfil(userID, competenceID) {
     }
 }
 
-
-
 async function suppProfil(userID, competenceID) {
     try {
         // Recherche le profil à supprimer
@@ -47,8 +47,52 @@ async function suppProfil(userID, competenceID) {
     }
 }
 
+async function getEmployeInCategorie(competenceID) {
+    const employes = [];
+  
+    try {
+      let utilisateurs = await ProfilEmployerEtClient.find({ competenceID: competenceID });
+  
+      // Utilisez la méthode map pour itérer sur les utilisateurs avec des opérations asynchrones
+      await Promise.all(
+        utilisateurs.map(async (utilisateur) => {
+          try {
+            let utilisateurDetail = await Utilisateur.findById(utilisateur.utilisateurID);
+  
+            // Vérifiez si l'utilisateur a un rôle égal à 2 avant d'ajouter au tableau
+            if (utilisateurDetail && utilisateurDetail.role === 2) {
+              employes.push(utilisateurDetail);
+            }
+          } catch (error) {
+            console.error('Erreur lors de la récupération des détails de l\'utilisateur :', error.message);
+          }
+        })
+      );
+  
+      return employes;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+  
+  async function getAllEmployeesInService(serviceId) {
+    const employees = [];
+    try {
+        let competences = await service.getAllCompetencesInService(serviceId);
+        for(let i=0; i<competences.length;i++) {
+            let employesDuService = await getEmployeInCategorie(competences[i]._id);
+            employees.push(...employesDuService); 
+        }
+        return employees;
+    }catch(err) {
+        throw new Error(err.message);
+    }
+  }
+
 
 module.exports = {
     ajouterProfil,
-    suppProfil
+    suppProfil,
+    getEmployeInCategorie,
+    getAllEmployeesInService
 };
