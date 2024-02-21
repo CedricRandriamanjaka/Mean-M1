3133aedb4fec79b866487ce32d6c7a4780c4ce28
MihobyF <mihobyfahasoavana@gmail.com>
2024-02-21 22:25:29 +0100

    getDepense avec filtre

--- routes/depense.js
+++ routes/depense.js
@@ -1,51 +1,64 @@
-var express = require('express');
+var express = require("express");
 const router = express.Router();
 
-const DepenseService = require('../services/depense');
+const DepenseService = require("../services/depense");
 const depenseService = new DepenseService();
 
-router.post('/types', async (req, res) => {
-    const data = req.body;
-  
-    try {
-      const result = await depenseService.ajoutTypeDepense(data.nomDepense);
-      res.status(201).json(result);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
+router.post("/types", async (req, res) => {
+  const data = req.body;
 
-  router.post('/', async (req, res) => {
-    const data = req.body;
-  
-    try {
-      const result = await depenseService.ajoutDepense(data.description, data.montant, data.date, data.depenseTypeID);
-      res.status(201).json(result);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
+  try {
+    const result = await depenseService.ajoutTypeDepense(data.nomDepense);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-  router.get('/types', async (req, res) => {
-  
-    try {
-      const depenseType = await depenseService.getTypeDepense();
-      res.status(200).json(depenseType);
-    } catch (error) {
-      res.status(404).json({ message: error.message });
-    }
-  });
+router.post("/", async (req, res) => {
+  const data = req.body;
 
-  router.get('/', async (req, res) => {
-  
-    try {
-      const depense = await depenseService.getDepense();
-      res.status(200).json(depense);
-    } catch (error) {
-      res.status(404).json({ message: error.message });
-    }
-  });
+  try {
+    const result = await depenseService.ajoutDepense(
+      data.description,
+      data.montant,
+      data.date,
+      data.depenseTypeID
+    );
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
+router.get("/types", async (req, res) => {
+  try {
+    const depenseType = await depenseService.getTypeDepense();
+    res.status(200).json(depenseType);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.get("/types/:id", async (req, res) => {
+  const {id} = req.params;
+  try {
+    const depenseType = await depenseService.getTypeDepenseById(id);
+    res.status(200).json(depenseType);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.get("/mois=:mois/jour=:jour", async (req, res) => {
+  const mois = parseInt(req.params.mois, 10);
+  const jour = parseInt(req.params.jour, 10);
+  try {
+    const depense = await depenseService.getDepense(mois, jour);
+    res.status(200).json(depense);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 module.exports = router;
-
--- services/depense.js
+++ services/depense.js
@@ -3,14 +3,13 @@ const DepenseType = require("../models/depenceType");
 
 class DepenseService {
   async ajoutDepense(description, montant, date, depenseType) {
-
     if (!description || !montant || !date) {
       throw new Error("Veuillez remplir tous les champs.");
     }
 
     try {
       const depense = new Depense({
-        depenseType,
+        depenseTypeID: depenseType,
         description,
         montant,
         date,
@@ -23,10 +22,39 @@ class DepenseService {
     }
   }
 
-  async getDepense() {
+  async getDepense(mois, jour) {
+    console.log(mois);
     try {
-      const depenses = await Depense.find();
-      return depenses;
+      const filterExpr = {};
+      filterExpr.$and = [];
+
+      if (mois !== 0) {
+        filterExpr.$and.push({ $eq: [{ $month: "$date" }, mois] });
+      }
+
+      if (jour !== 0) {
+        filterExpr.$and.push({ $eq: [{ $dayOfMonth: "$date" }, jour] });
+      }
+
+      const depenses = await Depense.find({
+        $expr: filterExpr,
+      }).populate("depenseTypeID");
+      for (let i = 0; i < depenses.length; i++) {
+        console.log("date: " + depenses[i].date.toISOString().split("T")[0]);
+      }
+
+      // Formatter la date dans chaque objet du tableau
+      const depensesFormatees = depenses.map((depense) => ({
+        _id: depense._id,
+        depenseTypeID: depense.depenseTypeID,
+        date: depense.date.toISOString().split("T")[0],
+        description: depense.description,
+        montant: depense.montant,
+      }));
+
+      console.log(depensesFormatees);
+
+      return depensesFormatees;
     } catch (error) {
       throw new Error(error.message);
     }
@@ -41,21 +69,30 @@ class DepenseService {
     }
   }
 
+  async getTypeDepenseById(id) {
+    try {
+      const types = await DepenseType.findById(id);
+      return types;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
   async ajoutTypeDepense(nomDepense) {
     if (!nomDepense) {
-        throw new Error("Veuillez remplir tous les champs.");
-      }
-  
-      try {
-        const depenseType = new DepenseType({
-            nomDepense,
-        });
-        await depenseType.save();
-  
-        return { success: true, message: "Type Depense created." };
-      } catch (error) {
-        throw new Error(error.message);
-      }
+      throw new Error("Veuillez remplir tous les champs.");
+    }
+
+    try {
+      const depenseType = new DepenseType({
+        nomDepense,
+      });
+      await depenseType.save();
+
+      return { success: true, message: "Type Depense created." };
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 }
 
