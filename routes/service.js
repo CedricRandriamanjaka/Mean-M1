904e8e3080a632e3e45f1500c705386fde08a7d9
CedricRandriamanjaka <randriamanjakacedric@gmail.com>
2024-02-27 21:40:09 +0300

    liste

--- routes/service.js
+++ routes/service.js
@@ -23,6 +23,35 @@ router.post('/', async (req, res) => {
       res.status(500).json({ message: error.message });
     }
   });
+
+  router.get('/nonSpecial', async (req, res) => {
+    try {
+      const services = await serviceService.getServicesNonSpecial();
+      res.status(200).json(services);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  router.get('/serviceSpecial/', async (req, res) => {
+    try {
+      const services = await serviceService.getServiceSpecial();
+      res.status(200).json(services);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  router.get('/serviceCompetence/:idUser', async (req, res) => {
+    const { idUser } = req.params;
+
+    try {
+      const services = await serviceService.getServiceComp(idUser);
+      res.status(200).json(services);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
   
   router.get('/:id', async (req, res) => {
     const { id } = req.params;
